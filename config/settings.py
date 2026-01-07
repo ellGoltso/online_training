9c379d4a3a6501e3da29c938f9fa5f80d3f5285d
ellGoltso <goltsovi3@gmail.com>
2026-01-07 19:37:33 +0300

    amendments num 6

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("ENGINE"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
