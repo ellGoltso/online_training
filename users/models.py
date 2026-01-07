9d9b226a6cf4813b6a6167d142580c58afc5d8c8
ellGoltso <goltsovi3@gmail.com>
2026-01-07 19:33:47 +0300

    amendments num 5

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from materials.models import Lesson, Course
 class User(AbstractUser):
     """Модель пользователя"""
 
-    username = models.CharField(unique=True)
+    username = models.CharField(max_length=55, unique=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=50, blank=True, null=True)
