1bba855d0875a59e209dbaf833adb674ba30e524
ellGoltso <goltsovi3@gmail.com>
2026-01-09 16:17:44 +0300

    amendments num 11

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.162.12', '127.0.0.1', 'localhost']
 
 # Application definition
 
